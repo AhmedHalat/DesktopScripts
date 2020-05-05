ef1f8651949dfe5e2892b4b0669f6cb894551613
Ahmed Halat <ahmedhalat@gmail.com>
2020-05-05 05:14:48 -0400

    Fixed var name and added comments

--- VolumeCircle.sh
+++ VolumeCircle.sh
@@ -3,6 +3,8 @@
 osascript -e '
 
 set vol to output volume of (get volume settings)
-set axe to round((vol/100) * 51)
-get character axe of "abcedfghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXY"
+# Get volume as a percent of 51 characters
+set ang to round((vol/100) * 51)
+# Get the character to represent the angle the volume circle should be at
+get character ang of "abcedfghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXY"
 '
